d8c1783d17046820320f7acace0ac8e5249f2682
Jamilla Morgan <jamillamorgan02@gmail.com>
2023-03-14 00:06:18 -0500

    Edited forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,14 +1,17 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileRequired, FileAllowed
-from wtforms import StringField, TextAreaField
+from wtforms import StringField, TextAreaField, SelectField
 from wtforms.validators import InputRequired
 
 
 class MyForm(FlaskForm):
-    firstname = StringField('First Name', validators=[InputRequired()])
-    lastname = StringField('Last Name', validators=[InputRequired()])
-    email = StringField('Email', validators=[InputRequired(), Email()])
-    message = TextAreaField('Message', validators=[InputRequired()])
+    title = StringField('Property Title', validators=[InputRequired()])
+    location = StringField('Location', validators=[InputRequired()])
+    rooms = StringField('No. of Rooms', validators=[InputRequired()])
+    bathrooms = StringField('No. of Bathrooms', validators=[InputRequired()])
+    price = StringField('Price', validators=[InputRequired()])
+    desc = TextAreaField('Description', validators=[InputRequired()])
+    prop_type = SelectField(u'Property Type', choices=[('h', 'House'), ('a', 'Apartment')], validators=[InputRequired()])
 
 
 class PhotoForm(FlaskForm):
